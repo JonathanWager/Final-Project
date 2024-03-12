77555ea720b221248c15d804dc8f60f2c1cd571e
JonathanWager <storfot32@gmail.com>
2024-03-12 14:15:45 +0100

    Commit1: added://splashscreen with the color blue

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -8,6 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isAnimating = false
+    
+    var body: some View {
+        NavigationStack{
+            ZStack{
+                if isAnimating{
+                    MainView()
+                }
+                else{
+                    SplashScreenView()
+                        .transition(.opacity)
+                }
+            }
+        }.task {
+            withAnimation(Animation.easeInOut(duration: 4.0)) {
+                isAnimating = true
+            }
+        }
+    }
+}
+
+struct MainView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +39,25 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+    }
+}
+
+struct SplashScreenView: View {
+    @State private var scale: CGFloat = 1.0
+
+    var body: some View {
+        ZStack {
+            Color.white
+            Circle()
+                .fill(Color.blue)
+                .scaleEffect(scale)
+                .frame(width: 100, height: 100)
+                .onAppear {
+                    withAnimation(Animation.easeInOut(duration: 2.0)) {
+                        scale = 10.0
+                    }
+                }
+        }
     }
 }
 
