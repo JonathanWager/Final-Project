e32cb88761b00fd7ee9e25d48e4e62a70ebded36
JonathanWager <storfot32@gmail.com>
2024-03-12 13:44:13 +0100

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Jonathan Wåger on 2024-03-12.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
